c3feea6eb7f2287d1e76bf4686ec1ab7545b3482
Venom12345672 <48963915+Venom12345672@users.noreply.github.com>
2019-04-18 22:06:45 +0500

    Infinite Flow All Screens UI

---
+++ 9 April Infinite Flow-All Screens/Assets/Scripts/LoadingScreenControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreenControl : MonoBehaviour {
+
+    public Slider slider;
+	// Use this for initialization
+	void Start () {
+       // slider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        slider.value = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        while(slider.value != 100)
+        {
+            slider.value += 0.005f;
+        }
+        
+		
+	}
+    void ValueChangeCheck()
+    {
+        print(slider.value);
+    }
+}
---
+++ 9 April Infinite Flow-All Screens/Assets/Scripts/ScreenLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScreenLoader : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void LoadLevel(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
