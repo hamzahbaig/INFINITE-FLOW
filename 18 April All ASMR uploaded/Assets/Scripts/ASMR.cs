2d63d964d7ded61ce7afd7e0e4bdb31a6cf1ea6c
Venom12345672 <48963915+Venom12345672@users.noreply.github.com>
2019-04-18 22:09:47 +0500

    All ASMR uploaded

---
+++ 18 April All ASMR uploaded/Assets/Scripts/ASMR.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+ 
+
+public class ASMR : MonoBehaviour {
+    public Image myImage;
+    public GameObject myGameObject;
+    public AudioClip myClip;
+    private AudioSource myAudioSource;
+    public int index;
+
+    void Start()
+    {
+        if(History.activeList[index] == true)
+        {
+            myImage.color = Color.white;
+        }
+    }
+
+
+
+    public void buttonPressed()
+    {
+        //Turning on ASMR
+        if(History.activeList[index] == false)
+        {
+            History.activeList[index] = true;
+            myImage.color = Color.white;
+            myGameObject = GameObject.Find("AudioManager");
+            myAudioSource = myGameObject.AddComponent<AudioSource>();
+            myAudioSource.clip = myClip;
+            myAudioSource.Play();
+            myAudioSource.loop = true;
+            print(History.activeList[index]);
+
+        }
+        // Turning off ASMR
+        else if(History.activeList[index] == true)
+        {
+           
+            myImage.color = Color.grey;
+            History.activeList[index] = false;
+            myGameObject = GameObject.Find("AudioManager");
+            AudioSource[] audioSources = myGameObject.GetComponents<AudioSource>();
+            foreach(AudioSource item in audioSources)
+            {
+                if(item.clip.name == myClip.name)
+                {
+                    Destroy(item);
+                }
+            }
+            print(History.activeList[index]);
+
+        }
+
+    }
+   
+
+}
---
+++ 18 April All ASMR uploaded/Assets/Scripts/screenChanger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class screenChanger : MonoBehaviour {
+
+    private Vector3 fp; // first touch position
+    private Vector3 lp; // last touch position
+    public Vector3 swipe;
+    private float dragDistance; // minimum distance for a swipe to be registered
+
+	// Use this for initialization
+	void Start () {
+        dragDistance = Screen.height * 15 / 100;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount == 1)
+        {
+            Touch touch1 = Input.GetTouch(0);
+            if(touch1.phase == TouchPhase.Began)
+            {
+                fp = touch1.position;
+            } else if (touch1.phase == TouchPhase.Ended)
+            {
+                lp = touch1.position;
+                swipe = lp - fp;
+                
+                if(swipe.x >= dragDistance)
+                {
+                    SceneManager.LoadScene(1);
+                }
+                if (swipe.x <= -dragDistance)
+                {
+                    SceneManager.LoadScene(1);
+                }
+            }
+        }
+		
+	}
+}
