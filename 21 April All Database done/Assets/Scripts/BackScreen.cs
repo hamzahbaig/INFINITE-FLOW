f99f2f62188bc8955abae268c2a0ec8fc8fa3310
Venom12345672 <48963915+Venom12345672@users.noreply.github.com>
2019-04-21 18:04:48 +0500

    DataBase Done

---
+++ 21 April All Database done/Assets/Scripts/BackScreen.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BackScreen : MonoBehaviour {
+
+    int index;
+    private void Awake()
+    {
+         index = SceneManager.GetActiveScene().buildIndex;
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        //ebug.Log(index);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Application.platform == RuntimePlatform.Android)
+        {
+
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                if(index == 0)
+                {
+                    Application.Quit();
+                }
+                // Login Screen
+                if(index == 1)
+                {
+                    SceneManager.LoadScene(0);
+                }
+                // Main Menu
+                if(index == 2)
+                {
+                    SceneManager.LoadScene(1);
+                }
+                // Loading Screen
+                if(index == 3)
+                {
+                    SceneManager.LoadScene(2);
+                }
+                // GameSettings
+                if(index == 4)
+                {
+                    SceneManager.LoadScene(2);
+                }
+                //TrophyRack
+                if(index == 5)
+                {
+                    SceneManager.LoadScene(2);
+                }
+                //Multiplayer1
+                if(index == 6)
+                {
+                    SceneManager.LoadScene(2);
+                }
+                //Multiplayer2
+                if(index == 7)
+                {
+                    SceneManager.LoadScene(6);
+                }
+                if(index == 8)
+                {
+                    SceneManager.LoadScene(1);
+                }
+                // SinglePlayer
+                if(index == 9)
+                {
+                    SceneManager.LoadScene(2);
+                    print("Best");
+                }
+
+            }
+        }
+	}
+}
---
+++ 21 April All Database done/Assets/Scripts/restartButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class restartButton : MonoBehaviour
+{
+    public gridTest temp1;
+    private bool drawMode;
+    private bool buttonPressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        drawMode = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit;
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase== TouchPhase.Began)
+        {
+            //Debug.Log("REFREESHHHH MOTHERFUCCKKEERRRR");
+            Vector3 temp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            hit = Physics2D.Raycast(temp, Vector2.zero);
+            if (hit.collider != null && hit.transform.gameObject.tag == "refresh")
+            {
+                if (!buttonPressed)
+                {
+                    temp1.DestroyPattern();
+                    drawMode = false;
+                    temp1.PatternGenerate(4);
+                    buttonPressed = true;
+                    Invoke("RefreshPattern", 2);
+                }
+                
+            }
+        }
+        if (drawMode)
+        {
+            temp1.PatternDraw();            
+        }
+    }
+    public void RefreshPattern()
+    {
+        temp1.DestroyPattern();
+        drawMode = true;
+        buttonPressed = false;
+    }
+}
